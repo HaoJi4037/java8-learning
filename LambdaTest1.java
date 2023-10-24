2528b3b510363b801e304d27b4b450abe4e14330
alex.ji <alex.ji@klook.com>
2023-10-24 20:49:05 +0800

    lamda练习

---
+++ LambdaTest1.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class LambdaTest1 {
+
+
+    public Set<Integer> getAgeSet(List<User> users) {
+        Set<Integer> ageSet = new HashSet<>();
+        for (User user : users) {
+            ageSet.add(user.getAge());
+        }
+        return ageSet;
+    }
+
+    public Set<Integer> getAgeSet2(List<User> users) {
+        return users.stream()
+                .map(User::getAge)
+                .collect(Collectors.toSet());
+    }
+}
---
+++ UserTest.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    List<User> users;
+
+    @BeforeEach
+    void setUp() {
+        User user = new User(1L, "zhangsan1", 18);
+        User user2 = new User(2L, "zhangsan2", 19);
+        User user3 = new User(3L, "zhangsan3", 20);
+        users = Arrays.asList(user, user2, user3);
+    }
+
+
+    @Test
+    public void testAgeSet1() {
+        LambdaTest1 test1 = new LambdaTest1();
+        Set<Integer> actualAgeSet = test1.getAgeSet(users);
+        Set<Integer> expectedAgeSet = Set.of(18, 19, 20);
+        assertEquals(actualAgeSet, expectedAgeSet);
+    }
+
+    @Test
+    public void testAgeSet2() {
+        LambdaTest1 test1 = new LambdaTest1();
+        Set<Integer> actualAgeSet = test1.getAgeSet2(users);
+        Set<Integer> expectedAgeSet = Set.of(18, 19, 20);
+        assertEquals(actualAgeSet, expectedAgeSet);
+    }
+
+    @Test
+    public void testP() {
+        int count = 10000000;
+        {
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < count; i++) {
+                LambdaTest1 test1 = new LambdaTest1();
+                Set<Integer> actualAgeSet = test1.getAgeSet(users);
+            }
+            long end = System.currentTimeMillis();
+            System.out.println("testAgeSet1:" + (end - start));
+        }
+
+        {
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < count; i++) {
+                LambdaTest1 test1 = new LambdaTest1();
+                Set<Integer> actualAgeSet = test1.getAgeSet2(users);
+            }
+            long end = System.currentTimeMillis();
+            System.out.println("testAgeSet2:" + (end - start));
+        }
+
+    }
+}
